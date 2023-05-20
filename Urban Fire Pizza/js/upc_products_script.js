0026a64c7abbdc8a38e54fa4c10bfa9327685212
Niraj0day <134109660+Niraj0day@users.noreply.github.com>
2023-05-19 19:10:18 -0600

    Add files via upload

---
+++ Urban Fire Pizza/js/upc_products_script.js
@@ -0,0 +1,22 @@
+// JavaScript Document<script>
+ $(document).ready(function() {
+     $(".addoncheck").hide();
+     $(".productcheck").hide();
+     $(".crustaddoncheck").hide();
+     $(".sauceaddoncheck").hide();
+     $("div[id*=addon]").click(function () {
+        $(this).children(".addoncheck").toggle();
+    });
+     $("div[id*=product]").click(function () {
+     	$(".productcheck").hide();
+        $(this).children(".productcheck").toggle();
+    });
+     $("div[id*=crust]").click(function () {
+     	$(".crustaddoncheck").hide();
+        $(this).children(".crustaddoncheck").toggle();
+    });
+     $("div[id*=sauce]").click(function () {
+     	$(".sauceaddoncheck").hide();
+        $(this).children(".sauceaddoncheck").toggle();
+    });
+}); 
